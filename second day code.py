db5d569a62c9ce6fb277bd67678dd6c0ea2b062f
naman-sharma-git <nishusharma0917@gmail.com>
2025-09-04 23:23:22 +0530

    Add files via upload

---
+++ second day code.py
@@ -0,0 +1,18 @@
+name = "naman"
+age = 18
+
+print(age)
+
+print("my name is : ", name)
+
+print("my age is :", age)
+
+print(type(name))
+print(type(age))
+
+age = 18
+old = False 
+a = None
+
+print(type(old))
+print(type(a))
