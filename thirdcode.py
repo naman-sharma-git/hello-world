2ebec3d405ba610bac66cefd9a24a2220ccffa6f
naman sharma <nishusharma0917@gmail.com>
2025-09-06 22:53:51 +0530

    one more step on coding

---
+++ thirdcode.py
@@ -0,0 +1,11 @@
+str1 = "this is a string. we are ctreating in python."
+print(str1)
+str2 = "this is a string. \n we are creating in python"
+print(str2)
+str1 = "naman"
+str2 = "sharma"
+print(str1+str2)
+
+str1 = 2
+str2 = 5
+print(str1 + str2)
