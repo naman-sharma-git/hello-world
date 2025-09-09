f2166af6617d2235dd218c6c087df590891b949e
naman sharma <nishusharma0917@gmail.com>
2025-09-09 23:40:44 +0530

    latenightwork

--- thirdcode.py
+++ thirdcode.py
@@ -9,3 +9,104 @@ print(str1+str2)
 str1 = 2
 str2 = 5
 print(str1 + str2)
+str1 = "python"
+print(len(str1))
+str1 = "python"
+str2 = "coding"
+final_str = str1 + " " + str2
+print(final_str)
+
+
+
+
+str = "naman sharma"
+ch = str[0]
+print(ch)
+
+str = "naman sharma"
+ch = str[3]
+print(ch)
+
+
+str = "naman sharma"
+ch = str[5]
+print(ch)
+
+
+str = "naman sharma"
+print(str[1:4])
+
+str = "naman sharma"
+print(str[1:5])
+
+str = "naman sharma"
+print(str[1:len(str)])
+
+
+str = "naman sharma"
+print(str[1:])
+
+
+str = "apple"
+print(str[-3:-1])
+
+str = "I am a code learner"
+print(str.endswith("ner"))
+
+
+str = "I am a code learner"
+print(str.endswith("lea"))
+
+
+str = "i am a code learner"
+print(str.capitalize())
+print(str)
+
+
+str = "i am a code learner"
+print(str.replace("o", "a"))
+
+
+str = "i am a code learner"
+print(str.replace("a", "o"))
+
+str = "i am a code learner"
+print(str.replace("learner", "expert"))
+
+str = "i am a code learner"
+print(str.find("l"))
+
+
+str = "i am a code learner"
+print(str.find("code"))
+
+
+str = "i am a code learner"
+print(str.find("x"))
+
+
+str = "i am a code learner from apna college"
+print(str.count("from"))
+
+
+str = "i am a code learner from apna college"
+print(str.count("a"))
+
+
+
+
+
+str = "good night"
+print(str)
+
+
+
+
+
+
+
+
+
+
+
+
