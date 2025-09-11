7570b1af082e9e90d4a3cb7911c83a53660a84c5
naman sharma <nishusharma0917@gmail.com>
2025-09-11 23:20:09 +0530

    need job

---
+++ pythoncode.py
@@ -0,0 +1,65 @@
+name = input("entre your name : ")
+print("length of your name is", len(name))
+
+str = "Hi, $Iam the $ symbol $99.99"
+print(str.count("$"))
+
+
+
+age = 21
+if(age >= 18):
+    print("can vote & apply for license")
+    
+
+age = 25
+if (age >= 18):
+        print("can vote")
+        print("can drive")
+
+
+light = "green"
+
+if(light == "red"):
+      print("stop")
+
+elif(light == "green"):
+      print("go")
+
+elif(light == "yellow"):
+      print("look")
+
+
+num = 5 
+
+if(num > 2):
+      print("greater than 2")
+elif(num > 3):
+      print ("greater than 3")
+
+
+
+light = "yellow"
+
+if(light == "red"):
+      print("stop")
+
+elif(light == "green"):
+      print("go")
+
+elif(light == "yellow"):
+      print("look")
+
+else :
+      print("light is broken")
+
+
+age = 14
+
+if (age >= 18):
+      print("can vote")
+else:
+      print("cannot vote") #indentation {}
+
+
+print("the end for today")
+
