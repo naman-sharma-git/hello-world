343dea7b663fee57c20d056e5c31cbb242ebac42
naman sharma <nishusharma0917@gmail.com>
2025-09-21 00:38:21 +0530

    damage

---
+++ loopcode.py
@@ -0,0 +1,146 @@
+count = 1 
+while count <= 5 :
+    print("hello")
+    count += 1 
+
+print(count)
+
+
+
+i = 1 
+while i <= 5 :
+    print("python")
+    i += 1 
+
+
+u = 1 
+while u <= 50 :
+    print("VScode")
+    u += 1 
+
+
+#i = 1 
+#while i <= 100000 :
+    print("namansharma", i)
+    i += 1   
+
+
+#print 1 to 5 
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+print("loop ended")
+
+
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
+
+print("loop ended")
+
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+i = 1 
+while i <= 10:
+    print(3*i)
+    i += 1    
+
+
+i = 1 
+while i <= 10:
+    print(8*i)
+    i += 1    
+
+
+
+i = 1 
+while i <= 10:
+    print(28*i)
+    i += 1    
+
+
+i = 1 
+while i <= 10:
+    print(28*i)
+    i += 1
+
+
+
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+idx = 0 
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
+
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+heros = ["sanjaydutt", "ironman", "bobydawal", "salmankhan", "thor"]
+
+idx = 0 
+while idx < len(heros):
+    print(heros[idx])
+    idx += 1
+
+
+
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+x = 36
+
+i = 0 
+while i < len(nums):
+    if(nums[i] == x):
+        print("FOUND at idx", i)
+    i += 1
+
+
+
+i = 0 
+while i < len(nums):
+    if(nums[i] == x):
+        print("FOUND at idx", i)
+    else:
+            print("finding....")
+    i += 1
+
+
+#i = 1
+#while i <= 5:
+    #print(i)
+    #if(i==3):
+        #break
+   # i += 1
+
+#print("end of loop")
+
+
+
+#i = 0 
+#while i < len(nums):
+    #if(nums[i] == x):
+       # print("FOUND at idx", i)
+       # break
+   # else:
+            #print("finding....")
+   # i += 1
+
+p#rint("end of loop")
+
+
+
