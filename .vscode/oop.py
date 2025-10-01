4553c998b8b3ba96253bd632f5528fb435494a45
naman sharma <nishusharma0917@gmail.com>
2025-10-02 02:55:06 +0530

    oop for python

---
+++ .vscode/oop.py
@@ -0,0 +1,63 @@
+a = 10 
+b = 20
+
+sun = a + b
+print(sum)
+
+
+diff = a - b
+print(diff)
+
+
+class Student:
+    name = "naman"
+
+s1 = Student()
+print(s1)
+
+
+
+
+class Student:
+    name = "naman"
+
+s1 = Student()
+print(s1.name)
+
+
+s2 = Student()
+print(s2.name)
+
+
+class car:
+    color = "blue"
+
+car1 = car()
+print(car1.color)
+
+
+
+class car:
+    color = "blue"
+    brand = "BMW"
+
+car1 = car()
+print(car1.color)
+print(car1.brand)
+
+
+class Student:
+    name = "naman"
+    def __init__(self):
+        print(self)
+        print("adding new student in database..")
+
+
+s1 = Student()
+print(s1)
+
+
+
+
+#class Student:
+   # name = "naman"
