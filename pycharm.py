eab86f4423aa65df6f99c6036eba8f1b1bf7be9e
naman-sharma-git <nishusharma0917@gmail.com>
2025-10-23 00:37:04 +0530

    it's a order

---
+++ pycharm.py
@@ -0,0 +1,148 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+1000
+1000
+-657
+-657
+0
+0
+
+98.4
+98.4
+
+-6.54
+-6.54
+
+
+"hello"
+'hello'
+
+'hello'
+'hello'
+
+'''hello'''
+'hello'
+
+"""hello"""
+'hello'
+
+
+"hello everyone we are learning python
+SyntaxError: unterminated string literal (detected at line 1)
+"hello everyone we are learning python"
+'hello everyone we are learning python'
+
+
+"hello,
+SyntaxError: unterminated string literal (detected at line 1)
+"""hello,
+how are you?
+let's learn python"""
+"hello,\nhow are you?\nlet's learn python"
+
+
+True
+True
+
+False
+False
+
+None
+
+
+ a=1
+ 
+SyntaxError: unexpected indent
+a=1
+print(a)
+1
+
+
+a=3
+b=4
+c=1
+print(a+b+c)
+8
+
+a=5
+b=9
+c=6
+d=a + b + c
+print(d)
+20
+
+del d
+
+print(d)
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    print(d)
+NameError: name 'd' is not defined. Did you mean: 'id'?
+print(a)
+5
+
+
+x=6
+print(x)
+6
+
+x=3
+print(x)
+3
+
+
+x=3
+y=5
+z=x*y
+print(z)
+15
+
+
+
+
+a="python"
+print(a)
+python
+
+
+
+>>> 
+>>> a=1
+>>> print(a)
+1
+>>> 
+>>> 
+>>> _a=4
+>>> print(_a)
+4
+>>> 
+>>> 
+>>> age=54
+>>> print(age)
+54
+>>> 
+>>> 
+>>> a="hello world"
+>>> print(a)
+hello world
+>>> 
+>>> 
+>>> 
+>>> a="hello"
+>>> b=" world"
+>>> print(a+b)
+hello world
+>>> 
+>>> 
+>>> 
+>>> import keyword
+>>> leyword.kwlist
+Traceback (most recent call last):
+  File "<pyshell#103>", line 1, in <module>
+    leyword.kwlist
+NameError: name 'leyword' is not defined. Did you mean: 'keyword'?
+>>> 
+... import keyword
+>>> import keyword
+>>> keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
