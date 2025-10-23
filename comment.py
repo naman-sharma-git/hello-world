ac6769ff8ac34ef8a3823c5a5292d6f8e299e2a6
naman-sharma-git <nishusharma0917@gmail.com>
2025-10-24 00:28:13 +0530

    tuedude

---
+++ comment.py
@@ -0,0 +1,29 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> x = 1000
+>>> y = 7443
+>>> 
+>>> # adding 2 numbers
+>>> z = x + y
+>>> z
+8443
+>>> 
+>>> name = "mark" # name of the person
+>>> name
+'mark'
+>>> 
+>>> # age = 25
+>>> age
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    age
+NameError: name 'age' is not defined
+>>> 
+>>> """we are writing a multiline comment.
+... we will assign age of mark in variable called age.
+... that is it"""
+'we are writing a multiline comment.\nwe will assign age of mark in variable called age.\nthat is it'
+>>> 
+>>> age = 33
+>>> age
+33
