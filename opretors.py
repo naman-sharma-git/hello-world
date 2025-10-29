fa01fdf3a4827c22e5472405296e7d632f5e0adc
naman-sharma-git <nishusharma0917@gmail.com>
2025-10-30 00:35:51 +0530

    +=- etc

---
+++ opretors.py
@@ -0,0 +1,106 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x = 10
+
+y = x + 20
+y
+30
+
+#arithmetic operator
+
+num1 = 10
+num2 = 5
+
+num1
+10
+num2
+5
+
+# + -> addition
+num1 + num2
+15
+
+
+# - -> subtraction
+
+num1 - num2
+5
+
+# * -> multiplication
+
+num1 * num2
+50
+
+
+# / => division
+
+num1 / num2
+2.0
+
+
+# // => floor division
+num1 // num2
+2
+
+10/3
+3.3333333333333335
+
+10 // 3
+3
+
+# % => modulus
+
+num1 % num2
+0
+
+10 % 3
+1
+
+# ** -> exponent
+
+5 ** 2
+25
+
+3 ** 4
+81
+
+# assignment opperator
+>>> 
+>>> value = 100
+>>> 
+>>> add = num1 + num2
+>>> add
+15
+>>> 
+>>> x = 10
+>>> 
+>>> x
+10
+>>> 
+>>> 
+>>> x += 1 # x= x+1
+>>> 
+>>> x
+11
+>>> 
+>>> x -= 6
+>>> x -= 6 # x = x-6
+>>> 
+>>> x
+-1
+>>> 
+>>> x -= 6 # x = x - 6
+>>> x
+-7
+>>> 
+>>> x = 11
+>>> x
+11
+>>> 
+>>> x *= 10 # x = x / 20
+>>> x
+110
+>>> x /= 20
+>>> x
+5.5
+>>> 
