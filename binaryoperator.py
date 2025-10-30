5f97acc9862743e1e5c386d1d9bcaa7dc2b9f031
naman-sharma-git <nishusharma0917@gmail.com>
2025-10-30 23:13:33 +0530

    types of operators

---
+++ binaryoperator.py
@@ -0,0 +1,60 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#precedence
+
+
+5 + 10 * 6
+65
+
+name = 'mark'
+age = 33
+
+name
+'mark'
+
+age
+33
+
+name == "mark" or name == "johm" and age < 18
+True
+
+
+>>> {name == 'mark' or name == 'jhon'} and age < 18
+False
+>>> 
+>>> (5 + 10) * 6
+90
+>>> 
+>>> 100 / 10 * 10
+100.0
+>>> 
+>>> 
+>>> 2 ** 1 ** 3
+2
+>>> 
+>>> # associativity
+>>> 
+>>> 
+>>> ## clasification of operator based on the number of operands
+>>> 
+>>> # unery
+>>> 
+>>> -10
+-10
+>>> 
+>>> 10
+10
+>>> 
+>>> not True
+False
+>>> 
+>>> # binary operator
+>>> 
+>>> 10 - 5
+5
+>>> 
+>>> True or False
+True
+>>> 
+>>> # ternary operator
+>>> 
