fd5a92f6c7456bf3edb7e695292ed974294cc887
naman-sharma-git <nishusharma0917@gmail.com>
2025-11-02 19:58:50 +0530

    Add files via upload

---
+++ area_of_triangle.py
@@ -0,0 +1,12 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> """
+... when all the length of the slides of triangle is known as a, b, c
+... semi perimeter (s) = (a + b + c)/2
+... area = square root of (s* (s - a) * (s - b) * (s-c))
+... """
+... a = float(input("enter dirst side: "))
+... b = float(input("enter second side: "))
+... c = float(input("enter third side: "))
+... s = (a + b + c) / 2
+... area = ( s * (s - a) * (s - b) * (s - c))
