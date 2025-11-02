ed68f9b0f93e834efdd7a527e5edf0b7059e23fe
naman-sharma-git <nishusharma0917@gmail.com>
2025-11-03 02:03:33 +0530

    simple intrest

---
+++ simple_intrest.py
@@ -0,0 +1,16 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> """
+... simple intrest = (p * R * T) / 100
+... p = principal amount
+... r = rate of interest
+... t = time duration
+... """
+... 
+... 
+... principal = float(input("enter prinnncipal amount: "))
+... rate = float(input("enter interest rate: "))
+... time = float(input("enter time: "))
+... si = (principal * rate * time) / 100
+... print("simple intrest is", si)
+... KeyboardInterrupt
