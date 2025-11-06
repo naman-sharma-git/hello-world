d54a07cc0e4177099fa8e4210619d1c117985a0d
naman <nishusharma0917@gmail.com>
2025-11-07 00:49:41 +0530

    f using

---
+++ fstring.py
@@ -0,0 +1,23 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name = "John"
+... age = 20
+... language = "python"
+... hour = 3
+... #jhon is 20 year old
+... #print(name, "is", age, "yearold.")
+... 
+... #jhon is 20 year old he studies python 3 hours a day
+... print(name, "is", age, "year old. he studies",language, hour, "hours a day")
+... 
+... 
+... # using f-string
+... print(f"{name} is {age} years old. he studies {language} {hour} a day.")
+... 
+... 
+... sub1 = 75
+... sub2 = 87
+... sub3 = 87
+... 
+... print(f"{name} scored {sub1 + sub2 + sub3} marks in total")
+... percent = (sub1 + sub2 + sub3) / 3
