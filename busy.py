7f186c63d75eb96cc3ff197f0ab42f76a3b73218
naman <nishusharma0917@gmail.com>
2025-11-07 22:38:47 +0530

    busy in exam prepration

---
+++ busy.py
@@ -0,0 +1,16 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name = "naman sharma"
+>>> age = 19
+>>> print(name)
+naman sharma
+>>> print(age)
+19
+>>> 
+>>> print(my name is "naman sharma" )
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("my name is naman sharma")
+my name is naman sharma
+>>> 
+>>> print("i dont do program today because i am busy in exam prepration")
+i dont do program today because i am busy in exam prepration
