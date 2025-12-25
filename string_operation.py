1d904aa8f26a351921d6966556fc269b1734454e
naman <nishusharma0917@gmail.com>
2025-12-25 07:34:25 +0530

    operations like in

---
+++ string_operation.py
@@ -0,0 +1,29 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> s1 = "python is fun"
+... print(s1[0])
+... print(s1[-1])
+... print(len(s1))
+... 
+... language = "python"
+... version = "3.13.3"
+... print(language + version)
+... 
+... 
+... s1 = "python"
+... print(s1 * 3)
+... 
+... 
+... #membership operation
+... #in
+... s1 = "python is fun"
+... print("python" in s1)
+... print("i" in s1)
+... print("z" in s1)
+... print("java" in s1)
+... 
+... # not in
+... s1 = "python is fun"
+... print("python" not in s1)
+... print("z" not in s1)
+... print("java"  not in s1)
