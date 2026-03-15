558d73236f54034a339c900d7d3084250479ab59
naman <nishusharma0917@gmail.com>
2026-03-15 22:57:30 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,12 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #tuple
+... # (item1 , item2 ,...)
+... # seq of item as a collection
+... 
+... t1 = ("python" , 10,  1.5 , True , [1, 2, 3], (12, 32) )
+... print(len(t1))
+... 
+... #accessing item of a tuple - index
+... print(t1[0])
+... print(t1[1])
