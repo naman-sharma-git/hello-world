de228fb4bfe29266a3fad7d1127761a4daba84e1
naman <nishusharma0917@gmail.com>
2026-03-31 02:44:56 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,17 @@
+#a -----> 10 times
+
+l = ["mike" , 10.4 , 999]
+print(l)
+
+for x in l:
+    print(x)
+
+for i in ["quiet" , 12.4 , 986 , 88]:
+    print(i)
+
+for i in range(1 , 11):
+    print(i)
+
+for i in range(1 , 11):
+    print("i am learning python")
+    
